0742e68dd22a34efaef854bfc102ebd07eec431e
rjsaito <ryuta.saito.q3@dc.tohoku.ac.jp>
2025-05-21 17:36:14 +0900

    Wed, May 21, 2025  5:36:14 PM

--- src/shared/hodo.cpp
+++ src/shared/hodo.cpp
@@ -11,7 +11,7 @@ namespace ana_helper {
         std::pair<Double_t, Double_t> peak_n_sigma(5.0, 5.0);
         TF1 *f_prefit = new TF1("pre_fit_gauss", "gausn", peak_pos-peak_n_sigma.first*stdev, peak_pos+peak_n_sigma.second*stdev);
         f_prefit->SetParameter(1, peak_pos);
-        f_prefit->SetParameter(2, stdev);
+        f_prefit->SetParameter(2, stdev*0.5);
         h->Fit(f_prefit, "0QEMR", "", peak_pos-peak_n_sigma.first*stdev, peak_pos+peak_n_sigma.second*stdev);
         Double_t fit_center = f_prefit->GetParameter(1);
         Double_t fit_sigma  = f_prefit->GetParameter(2);
