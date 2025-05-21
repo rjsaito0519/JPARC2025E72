84074fa2158956e46479a892b279644dc933983f
rjsaito <ryuta.saito.q3@dc.tohoku.ac.jp>
2025-05-21 17:37:57 +0900

    Wed, May 21, 2025  5:37:57 PM

--- src/shared/hodo.cpp
+++ src/shared/hodo.cpp
@@ -8,6 +8,7 @@ namespace ana_helper {
 
         Double_t peak_pos = h->GetBinCenter(h->GetMaximumBin());
         Double_t stdev    = h->GetStdDev();
+        std::cout << stdev << std::endl;
         std::pair<Double_t, Double_t> peak_n_sigma(5.0, 5.0);
         TF1 *f_prefit = new TF1("pre_fit_gauss", "gausn", peak_pos-peak_n_sigma.first*stdev, peak_pos+peak_n_sigma.second*stdev);
         f_prefit->SetParameter(1, peak_pos);
@@ -41,6 +42,9 @@ namespace ana_helper {
         Double_t peak_pos = h->GetBinCenter(h->GetMaximumBin());
         Double_t stdev    = h->GetStdDev();
         std::pair<Double_t, Double_t> peak_n_sigma(2.0, 2.0);
+
+        std::cout << stdev << std::endl;
+
 
         Double_t evnum_within_range = h->Integral(
             h->FindBin(conf.tdc_search_range[conf.detector.Data()].first),
