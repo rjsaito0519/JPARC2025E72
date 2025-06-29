7bb5792c860a764b4b9d95316ff7c71438a1650e
rjsaito <ryuta.saito.q3@dc.tohoku.ac.jp>
2025-06-29 23:22:17 +0900

    Sun, Jun 29, 2025 11:22:17 PM

--- scripts/create_runlist.py
+++ scripts/create_runlist.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser(
     add_help=True,
 )
 parser.add_argument("run_num", type=int, help="Input run number")
-parser.add_argument("suffix",  type=str, help="Input suffix")
+parser.add_argument("suffix",  type=str, help="Input suffix", nargs='*')
 parser.add_argument('--dc', action="store_true", help='Set check to True')
 args = parser.parse_args()
 # ---------------------------------------------------------------------------
@@ -20,46 +20,49 @@ import os
 import shutil
 import conf
 prefix = "dc" if args.dc else "hodo"
+set1 = set(args.suffix)
+set2 = set(["0", "Pi_hdprm", "K_hdprm", "Pi_t0", "K_t0", "Pi_hdphc", "K_hdphc"])
 
-if args.suffix not in ["0", "Pi_hdprm", "K_hdprm", "Pi_t0", "K_t0", "Pi_hdphc", "K_hdphc"]:
+common_elements = set1.difference(set2)
+if common_elements or len(set1) == 0:
     print("please input correct suffix: 0, Pi/K_hdprm, , Pi/K_to, Pi/K_hdphc")
     sys.exit()
 
 # -- write conf file  -----------------------------------
-suffix = args.suffix
-suffix_head = suffix.split("_")[0]
-conf_dir = os.path.join(conf.analyzer_dir, "param", "conf")
-conf_target_file = f"{conf_dir}/analyzer_run{args.run_num:0=5}_{prefix}_{suffix_head}.conf"
-if not os.path.isfile(conf_target_file):
-    shutil.copy(os.path.join(conf_dir, "analyzer_e72_20250518.conf"), conf_target_file)
+for suffix in args.suffix:
+    suffix_head = suffix.split("_")[0]
+    conf_dir = os.path.join(conf.analyzer_dir, "param", "conf")
+    conf_target_file = f"{conf_dir}/analyzer_run{args.run_num:0=5}_{prefix}_{suffix_head}.conf"
+    if not os.path.isfile(conf_target_file):
+        shutil.copy(os.path.join(conf_dir, "analyzer_e72_20250518.conf"), conf_target_file)
 
-buf = []
-with open(conf_target_file) as f:
-    for line in f:
-        s_list = line.split()
-        if len(s_list) != 0 and s_list[0] == "USER:":
-            target = f"param/USER/UserParam_run{args.run_num:0=5}"
-            if not os.path.isfile(os.path.join(conf.analyzer_dir, target)):
-                shutil.copy(os.path.join(conf.analyzer_dir, "param/USER/UserParam_e72_20250518"), target)
-            s_list[1] = target
-        if prefix == "hodo":
-            if len(s_list) != 0 and s_list[0] == "HDPRM:" and suffix in ["Pi_hdprm", "K_hdprm", "Pi_t0", "K_t0", "Pi_phc", "K_hdphc"]:
-                target = f"param/HDPRM/HodoParam_run{args.run_num:0=5}_{suffix_head}"
-                if os.path.isfile(os.path.join(conf.analyzer_dir, target)):  
-                    s_list[1] = target
-            if len(s_list) != 0 and s_list[0] == "HDPHC:" and suffix in ["Pi_hdprm", "K_hdprm", "Pi_t0", "K_t0", "Pi_hdphc", "K_hdphc"]:
-                target = f"param/HDPHC/HodoPHC_run{args.run_num:0=5}_{suffix_head}"
-                if os.path.isfile(os.path.join(conf.analyzer_dir, target)):  
-                    s_list[1] = target
-        # elif prefix == "dc":
-        #     if len(s_list) != 0 and s_list[0] == "DCTDC:" and suffix in ["Pi_1", "K_1", "Pi_2", "K_2", "Pi_3", "K_3"]:
-        #         s_list[1] = "param/DCTDC/DCTdcParam_run{:0=5}_{}".format(args.run_num, suffix[:suffix.find("_")])
-        buf.append(s_list)
+    buf = []
+    with open(conf_target_file) as f:
+        for line in f:
+            s_list = line.split()
+            if len(s_list) != 0 and s_list[0] == "USER:":
+                target = f"param/USER/UserParam_run{args.run_num:0=5}"
+                if not os.path.isfile(os.path.join(conf.analyzer_dir, target)):
+                    shutil.copy(os.path.join(conf.analyzer_dir, "param/USER/UserParam_e72_20250518"), target)
+                s_list[1] = target
+            if prefix == "hodo":
+                if len(s_list) != 0 and s_list[0] == "HDPRM:" and suffix in ["Pi_hdprm", "K_hdprm", "Pi_t0", "K_t0", "Pi_phc", "K_hdphc"]:
+                    target = f"param/HDPRM/HodoParam_run{args.run_num:0=5}_{suffix_head}"
+                    if os.path.isfile(os.path.join(conf.analyzer_dir, target)):  
+                        s_list[1] = target
+                if len(s_list) != 0 and s_list[0] == "HDPHC:" and suffix in ["Pi_hdprm", "K_hdprm", "Pi_t0", "K_t0", "Pi_hdphc", "K_hdphc"]:
+                    target = f"param/HDPHC/HodoPHC_run{args.run_num:0=5}_{suffix_head}"
+                    if os.path.isfile(os.path.join(conf.analyzer_dir, target)):  
+                        s_list[1] = target
+            # elif prefix == "dc":
+            #     if len(s_list) != 0 and s_list[0] == "DCTDC:" and suffix in ["Pi_1", "K_1", "Pi_2", "K_2", "Pi_3", "K_3"]:
+            #         s_list[1] = "param/DCTDC/DCTdcParam_run{:0=5}_{}".format(args.run_num, suffix[:suffix.find("_")])
+            buf.append(s_list)
 
-with open(conf_target_file, mode='w') as f:
-    for l in buf:
-        f.write('\t'.join(str(item) for item in l))
-        f.write("\n")
+    with open(conf_target_file, mode='w') as f:
+        for l in buf:
+            f.write('\t'.join(str(item) for item in l))
+            f.write("\n")
 # ---------------------------------------------------------------------------
 
 # -- write runlist file  -----------------------------------
