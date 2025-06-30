090929fc2e68673ce986b954334cb090b06add6d
rjsaito <ryuta.saito.q3@dc.tohoku.ac.jp>
2025-06-30 12:25:50 +0900

    Mon, Jun 30, 2025 12:25:50 PM

--- src/BH2_PHC.cpp
+++ src/BH2_PHC.cpp
@@ -176,7 +176,7 @@ Int_t main(int argc, char** argv) {
     }
 
     conf.detector = "bh2";
-    conf.phc_de_range_min = 0.0;
+    conf.phc_de_range_min = 0.2;
     analyze(path, particle);
     return 0;
 }
