75cb75237ce83da376d271ff1f6c4c6af980275c
rjsaito <ryuta.saito.q3@dc.tohoku.ac.jp>
2025-11-21 10:16:37 +0900

    Fri, Nov 21, 2025 10:16:37 AM

--- scripts/create_runlist.py
+++ scripts/create_runlist.py
@@ -90,7 +90,7 @@ with open(runlist_target_file) as f:
         if len(s_list) != 0 and s_list[0] == "RUN:":
             buf.append(s_list)
             for suffix in args.suffix:
-                buf.append([f"run{args.run_num:0=5}_{suffix}:"])                    
+                buf.append([f"run{args.run_num:0=5}_{suffix}_in:"])
                 if prefix == "hodo":
                     buf.append(["  bin: ./bin/Hodoscope"])
                     buf.append([f"  conf: ./param/conf/{SUB_DIR}/analyzer_run{args.run_num:0=5}_{prefix}_{suffix_head}.conf"])
@@ -101,6 +101,7 @@ with open(runlist_target_file) as f:
                     buf.append([f"  conf: ./param/conf/{SUB_DIR}/analyzer_run{args.run_num:0=5}_{prefix}_{suffix_head}.conf"])
                     buf.append([f"  data: ./rawdata/run{args.run_num:0=5}.dat"])
                     buf.append(["  root: {}".format(os.path.join(conf.output_dir, f"{prefix}_in_run{args.run_num:0=5}_{suffix}.root"))])
+                    buf.append([f"run{args.run_num:0=5}_{suffix}_out:"])
                     buf.append(["  bin: ./bin/BcOutTracking"])
                     buf.append([f"  conf: ./param/conf/{SUB_DIR}/analyzer_run{args.run_num:0=5}_{prefix}_{suffix_head}.conf"])
                     buf.append([f"  data: ./rawdata/run{args.run_num:0=5}.dat"])
