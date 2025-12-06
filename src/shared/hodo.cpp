2c923168c85cb0ba67d332607c0c06ad7266128b
rjsaito <ryuta.saito.q3@dc.tohoku.ac.jp>
2025-12-06 17:49:02 +0900

    Sat, Dec  6, 2025  5:49:02 PM

--- src/shared/hodo.cpp
+++ src/shared/hodo.cpp
@@ -185,23 +185,24 @@ namespace ana_helper {
             delete f_prefit;
 
             // -- second fit -----
-            TF1 *f_fit_mip_g = new TF1( Form("mip_gauss_%s", h->GetName()), "gausn", par[1]-mip_n_sigma.first*par[2], par[1]+mip_n_sigma.second*par[2]);
+            Double_t fit_range_min = par[1]-mip_n_sigma.first*par[2] > mip_range_left ? par[1]-mip_n_sigma.first*par[2] : mip_range_left;
+            TF1 *f_fit_mip_g = new TF1( Form("mip_gauss_%s", h->GetName()), "gausn", fit_range_min, par[1]+mip_n_sigma.second*par[2]);
             f_fit_mip_g->SetParameter(1, par[1]);
             f_fit_mip_g->SetParLimits(1, mip_range_left, h->GetXaxis()->GetXmax());
             f_fit_mip_g->SetParameter(2, par[2]*0.9);
             f_fit_mip_g->SetLineColor(kOrange);
             f_fit_mip_g->SetLineWidth(2.0);
-            h->Fit(f_fit_mip_g, "0QEMR", "", par[1]-mip_n_sigma.first*par[2], par[1]+mip_n_sigma.second*par[2]);
+            h->Fit(f_fit_mip_g, "0QEMR", "", fit_range_min, par[1]+mip_n_sigma.second*par[2]);
             Double_t chi_square_g = f_fit_mip_g->GetChisquare();
             Double_t p_value_g = TMath::Prob(chi_square_g, f_fit_mip_g->GetNDF());
 
-            TF1 *f_fit_mip_l = new TF1( Form("mip_landau_%s", h->GetName()), "landaun", par[1]-mip_n_sigma.first*par[2], par[1]+mip_n_sigma.second*par[2]);
+            TF1 *f_fit_mip_l = new TF1( Form("mip_landau_%s", h->GetName()), "landaun", fit_range_min, par[1]+mip_n_sigma.second*par[2]);
             f_fit_mip_l->SetParameter(1, par[1]);
             f_fit_mip_l->SetParLimits(1, mip_range_left, h->GetXaxis()->GetXmax());
             f_fit_mip_l->SetParameter(2, par[2]*0.9);
             f_fit_mip_l->SetLineColor(kOrange);
             f_fit_mip_l->SetLineWidth(2.0);
-            h->Fit(f_fit_mip_l, "0QEMR", "", par[1]-mip_n_sigma.first*par[2], par[1]+mip_n_sigma.second*par[2]);
+            h->Fit(f_fit_mip_l, "0QEMR", "", fit_range_min, par[1]+mip_n_sigma.second*par[2]);
             Double_t chi_square_l = f_fit_mip_l->GetChisquare();
             Double_t p_value_l = TMath::Prob(chi_square_l, f_fit_mip_l->GetNDF());
 
