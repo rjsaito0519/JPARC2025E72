7100e0a23850f3ad40eedbb05656364a38e8e616
SAITO Ryuta <sryuta@cw01.cc.kek.jp>
2026-02-09 00:54:24 +0900

    Mon Feb  9 00:54:24 JST 2026

--- lib/conf_example.py
+++ lib/conf_example.py
@@ -1,3 +1,9 @@
-analyzer_dir = "set/analyzer/dir"
-param_dir    = "set/param/dir"
-output_dir   = "set/output/dir"
+# analyzer_dir = "/home/had/sryuta/analyzer/JPARC2025Feb"
+# param_dir = "/home/had/sryuta/analyzer/JPARC2025May/param"
+# output_dir = "/group/had/sks/Users/sryuta/JPARC2025May"
+
+# Example Configuration
+analyzer_dir = "/path/to/your/analyzer/JPARC2025E72"
+param_dir    = "/path/to/your/analyzer/JPARC2025E72/param"
+output_dir   = "/path/to/your/output/directory"
+data_dir     = "/path/to/your/data/results"
